20e14d5739265f9c708f4017137eed76c422277e
XutongLi <brianleelxt@gmail.com>
2021-02-01 14:35:20 +0000

    finish extendible hash table

--- src/hash/extendible_hash.cpp
+++ src/hash/extendible_hash.cpp
@@ -10,34 +10,40 @@ namespace cmudb {
  * array_size: fixed array size for each bucket
  */
 template <typename K, typename V>
-ExtendibleHash<K, V>::ExtendibleHash(size_t size) {}
+ExtendibleHash<K, V>::ExtendibleHash(size_t size)
+    : maxBucketSize(size), globalDepth(0), numBuckets(1) {
+        table.push_back(std::make_shared<Bucket>(0, size, 0));
+    }
 
 /*
  * helper function to calculate the hashing address of input key
+ * Different hash function results in different offset.
+ * There is localDepth test in test code, so I have to use std::hash.
  */
 template <typename K, typename V>
 size_t ExtendibleHash<K, V>::HashKey(const K &key) {
-    std::unique_ptr<char[]> buf(new char[sizeof(key)]);
-    memcpy(buf.get(), &key, sizeof(key));
-    return CityHash64(buf.get(), sizeof(key));
+//    std::unique_ptr<char[]> buf(new char[sizeof(key)]);
+//    memcpy(buf.get(), &key, sizeof(key));
+//    return CityHash32(buf.get(), sizeof(key)) & ((1 << globalDepth) - 1);
+    return std::hash<K>{}(key) & ((1 << globalDepth) - 1);
 }
 
 /*
  * helper function to return global depth of hash table
- * NOTE: you must implement this function in order to pass test
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetGlobalDepth() const {
-  return 0;
+    return globalDepth;
 }
 
 /*
  * helper function to return local depth of one specific bucket
- * NOTE: you must implement this function in order to pass test
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
-  return 0;
+    if (bucket_id >= static_cast<int>(table.size()))
+        return -1;
+    return table[bucket_id]->localDepth;
 }
 
 /*
@@ -45,7 +51,7 @@ int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetNumBuckets() const {
-  return 0;
+    return numBuckets;
 }
 
 /*
@@ -53,7 +59,15 @@ int ExtendibleHash<K, V>::GetNumBuckets() const {
  */
 template <typename K, typename V>
 bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
-  return false;
+    std::unique_lock<std::mutex> tableLock(mutex);
+    size_t index = HashKey(key);
+    for (auto entry : table[index]->entries) {
+        if (entry.flag == HASVAL && entry.key == key) {
+            value = entry.value;
+            return true;
+        }
+    }
+    return false;
 }
 
 /*
@@ -62,7 +76,16 @@ bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
  */
 template <typename K, typename V>
 bool ExtendibleHash<K, V>::Remove(const K &key) {
-  return false;
+    std::unique_lock<std::mutex> tableLock(mutex);
+    size_t index = HashKey(key);
+    for (auto &entry : table[index]->entries) {
+        if (entry.flag == HASVAL && entry.key == key) {
+            entry.flag = NONE;
+            -- table[index]->size;
+            return true;
+        }
+    }
+    return false;
 }
 
 /*
@@ -71,7 +94,53 @@ bool ExtendibleHash<K, V>::Remove(const K &key) {
  * global depth
  */
 template <typename K, typename V>
-void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {}
+void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {
+    std::unique_lock<std::mutex> tableLock(mutex);
+    size_t index = HashKey(key);
+    std::shared_ptr<Bucket> insertBucket = table[index];
+    for (auto &entry : insertBucket->entries) {
+        if (entry.flag == HASVAL && entry.key == key) { // update
+            entry.value = value;
+            return;
+        }
+    }
+
+    while (insertBucket->size == insertBucket->maxBucketSize) {
+        if (insertBucket->localDepth == globalDepth) {  // increase size of table
+            int length = table.size();
+            for (int i = 0; i < length; ++ i) {
+                table.push_back(table[i]);  // offsets with the same suffix point to the same bucket
+            }
+            ++ globalDepth;
+        }
+        size_t indexSt0 = index;    // index started with 0
+        size_t indexSt1 = index ^ (1 << insertBucket->localDepth);  // index started with 1
+        table[indexSt1] = std::make_shared<Bucket>(0, maxBucketSize, 0);
+        int localDepth = ++ table[indexSt0]->localDepth;
+        table[indexSt1]->localDepth = localDepth;
+        for (auto &entry : table[indexSt0]->entries) {  // redistribute the old bucket
+            if (entry.flag == NONE) continue;
+            size_t curIndex = HashKey(entry.key);
+            if ((curIndex & (1 << (localDepth - 1))) == (indexSt0 & (1 << (localDepth - 1))))   continue;
+            table[indexSt1]->entries[table[indexSt1]->size] = BucketEntry(entry.key, entry.value);
+            ++ table[indexSt1]->size;
+            -- table[indexSt0]->size;
+            entry.flag = NONE;
+        }
+        ++ numBuckets;
+        index = HashKey(key);
+        insertBucket = table[index];
+    }
+    for (auto &entry : insertBucket->entries) { // insert
+        if (entry.flag == NONE) {
+            entry.key = key;
+            entry.value = value;
+            entry.flag = HASVAL;
+            ++ insertBucket->size;
+            return;
+        }
+    }
+}
 
 template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
--- src/include/hash/extendible_hash.h
+++ src/include/hash/extendible_hash.h
@@ -12,29 +12,59 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <iostream>
 
 #include "hash/hash_table.h"
 #include "hash/city.h"
+
+#define HASVAL 1
+#define NONE 0
+#define DEBUG
 
 namespace cmudb {
 
 template <typename K, typename V>
 class ExtendibleHash : public HashTable<K, V> {
 public:
-  // constructor
-  ExtendibleHash(size_t size);
-  // helper function to generate hash addressing
-  size_t HashKey(const K &key);
-  // helper function to get global & local depth
-  int GetGlobalDepth() const;
-  int GetLocalDepth(int bucket_id) const;
-  int GetNumBuckets() const;
-  // lookup and modifier
-  bool Find(const K &key, V &value) override;
-  bool Remove(const K &key) override;
-  void Insert(const K &key, const V &value) override;
+      // constructor
+      ExtendibleHash(size_t size);
+      // helper function to generate hash addressing
+      size_t HashKey(const K &key);
+      // helper function to get global & local depth
+      int GetGlobalDepth() const;
+      int GetLocalDepth(int bucket_id) const;
+      int GetNumBuckets() const;
+      // lookup and modifier
+      bool Find(const K &key, V &value) override;
+      bool Remove(const K &key) override;
+      void Insert(const K &key, const V &value) override;
 
 private:
-  // add your own member variables here
+    struct BucketEntry {
+        BucketEntry() : flag(0) {}
+        BucketEntry(const K &_key, const V &_value) : flag(1), key(_key), value(_value) {}
+        int flag;   // 0 - empty, 1 - has value
+        K key;
+        V value;
+    };
+    struct Bucket {
+        Bucket(int _localDepth, size_t _maxBucketSize, size_t _size)
+            : localDepth(_localDepth), maxBucketSize(_maxBucketSize), size(_size) {
+            entries.assign(maxBucketSize, BucketEntry());
+        };
+        std::vector<BucketEntry> entries;
+        int localDepth;
+        size_t maxBucketSize;
+        size_t size;
+    };
+    // private members
+    size_t maxBucketSize;
+    int globalDepth;
+    int numBuckets;
+    std::vector<std::shared_ptr<Bucket>> table;
+    std::mutex mutex;
 };
 } // namespace cmudb
